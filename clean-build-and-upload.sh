9a0844a61490266691f195fb64fe9a97e3ff2ebe
Andrew Straw <strawman@astraw.com>
2009-05-07 13:41:40 -0700

    build .pdf and upload it

--- clean-build-and-upload.sh
+++ clean-build-and-upload.sh
@@ -1,1 +1,13 @@
-make clean && make html && ./rsync-it.sh
+#!/bin/bash
+
+# exit on error
+set -e
+
+rm -rf build
+make latex
+cd build/latex
+make all-pdf
+cd ../..
+make html
+
+./rsync-it.sh
--- rsync-it.sh
+++ rsync-it.sh
@@ -1,1 +1,2 @@
+rsync -avzP build/latex/motmot.pdf xen1:/var/websites/code.astraw.com/motmot.pdf
 rsync -avzP --delete build/html/ xen1:/var/websites/code.astraw.com/projects/motmot/
